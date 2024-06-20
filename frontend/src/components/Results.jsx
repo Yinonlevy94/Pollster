d063a248349e55a250a7e32c1392aedda0b0a7b7
Yinonlevy94 <91956854+Yinonlevy94@users.noreply.github.com>
2024-06-20 16:32:48 +0300

    lasto commito

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,10 +1,12 @@
-// frontend/src/components/Results.jsx
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './Results.css'; // Ensure this CSS file exists and is correctly referenced
+import Confetti from 'react-confetti';
+import './Results.css'; 
 
 const Results = () => {
   const [results, setResults] = useState(null);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
   useEffect(() => {
     const fetchResults = async () => {
@@ -19,6 +21,18 @@ const Results = () => {
     fetchResults();
   }, []);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   if (!results) {
     return <div className="loading">Loading...</div>;
   }
@@ -27,6 +41,11 @@ const Results = () => {
 
   return (
     <div className="results-container">
+      <Confetti
+        width={windowWidth}
+        height={windowHeight}
+        numberOfPieces={200}
+      />
       <h1>Election Results</h1>
       <div className="result">
         <h2>Winner</h2>
@@ -34,12 +53,12 @@ const Results = () => {
         <h1>Congratulations, {winner.name}!</h1>
         <p>You have won the election with {winner.votes} votes ({winner.percentage}%) and {winner.mandates} mandates.</p>
       </div>
-      <div className="result">
+      <div className="result second-place">
         <h2>Second Place</h2>
         <img src={secondPlace.img} alt={secondPlace.name} />
         <p>{secondPlace.name} received {secondPlace.votes} votes ({secondPlace.percentage}%) and {secondPlace.mandates} mandates.</p>
       </div>
-      <div className="result">
+      <div className="result third-place">
         <h2>Third Place</h2>
         <img src={thirdPlace.img} alt={thirdPlace.name} />
         <p>{thirdPlace.name} received {thirdPlace.votes} votes ({thirdPlace.percentage}%) and {thirdPlace.mandates} mandates.</p>
